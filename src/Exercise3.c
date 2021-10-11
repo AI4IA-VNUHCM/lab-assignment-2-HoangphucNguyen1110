f60e26304a2ce2d99acb6515ea1419123ae4487c
HoangphucNguyen1110 <80943005+HoangphucNguyen1110@users.noreply.github.com>
2021-10-11 18:14:19 +0700

    up ex 3(1)

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -9,13 +9,37 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+int product_prime(int n)
+{
+	int i=2;
+	while (n!=1)
+	{
+		if(n%i==0)
+		{
+			printf("%d",i);
+			n/=i;
+			if(n!=1)
+			{
+				printf("*");
+			}
+		}
+		
+		else
+		{
+			i++;
+		}	 
+	}
+}
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	
 	//Your codes here
-	
+	int n;
+	int i=2;
+	printf("Input a number: ");
+	scanf("%d",&n);
+	product_prime(n);
 	
 	return 0;
 }
